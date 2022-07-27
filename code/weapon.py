017402afd31096632ee85a15f7a2c15a5e35f78b
MuhammedAky <muhammedaky24@gmail.com>
2022-07-27 14:39:30 +0300

    UI

--- code/weapon.py
+++ code/weapon.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame 
 
 class Weapon(pygame.sprite.Sprite):
 	def __init__(self,player,groups):
@@ -8,11 +8,11 @@ class Weapon(pygame.sprite.Sprite):
 		# graphic
 		full_path = f'../graphics/weapons/{player.weapon}/{direction}.png'
 		self.image = pygame.image.load(full_path).convert_alpha()
-
+		
 		# placement
 		if direction == 'right':
 			self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,16))
-		elif direction == 'left':
+		elif direction == 'left': 
 			self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0,16))
 		elif direction == 'down':
 			self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))

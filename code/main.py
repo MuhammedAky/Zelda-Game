9f02f7e7343d1aee65dcb6af232eadb7e78eb17c
MuhammedAky <muhammedaky24@gmail.com>
2022-07-27 14:36:06 +0300

    weapons

--- code/main.py
+++ code/main.py
@@ -11,8 +11,8 @@ class Game:
 		pygame.display.set_caption('Zelda')
 		self.clock = pygame.time.Clock()
 
-		self.level = Level()
-
+		self.level = Level() 
+	
 	def run(self):
 		while True:
 			for event in pygame.event.get():

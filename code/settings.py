2e4c4c2a229cec73ef551bf917ceef8974fa27d3
MuhammedAky <muhammedaky24@gmail.com>
2022-07-28 18:19:53 +0300

    Fixes and audio

--- code/settings.py
+++ code/settings.py
@@ -3,6 +3,11 @@ WIDTH    = 1280
 HEIGTH   = 720
 FPS      = 60
 TILESIZE = 64
+HITBOX_OFFSET = {
+	'player': -26,
+	'object': -40,
+	'grass': -10,
+	'invisible': 0}
 
 # ui 
 BAR_HEIGHT = 20
@@ -23,7 +28,13 @@ HEALTH_COLOR = 'red'
 ENERGY_COLOR = 'blue'
 UI_BORDER_COLOR_ACTIVE = 'gold'
 
-# weapons
+# upgrade menu
+TEXT_COLOR_SELECTED = '#111111'
+BAR_COLOR = '#EEEEEE'
+BAR_COLOR_SELECTED = '#111111'
+UPGRADE_BG_COLOR_SELECTED = '#EEEEEE'
+
+# weapons 
 weapon_data = {
 	'sword': {'cooldown': 100, 'damage': 15,'graphic':'../graphics/weapons/sword/full.png'},
 	'lance': {'cooldown': 400, 'damage': 30,'graphic':'../graphics/weapons/lance/full.png'},
